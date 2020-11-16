07593708def906f5e56ef67a630cc2202235fa8f
z1069867141 <919824370@qq.com>
2020-11-16 18:54:28 +0800

    jenkiens 运行代码提交

---
+++ iSelenium_Python/Readme.md
@@ -0,0 +1,13 @@
+**Selenium 自动化测测试程序 (Jenkins使用) **
+运行环境：
+- selenium web driver
+- python3
+- pytest
+- git
+
+配置文件： iselenium.ini
+- 将配置文件复制到本地磁盘的[user.home]目录
+- 填入设备的chromwebdriver文件的全路径
+
+运行命令:
+pytest -v test/web_ut.py -o junit_family=xunit2 --junit-xml=pytests.xml
---
+++ iSelenium_Python/test/web_ut.py
@@ -0,0 +1,62 @@
+import configparser
+import os
+import time
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+
+class ISelenium(unittest.TestCase):
+    # 读入配置文件
+    def get_config(self):
+        '''
+        @config: 实例化配置方法
+        '''
+        conifg = configparser.ConfigParser()  # configparser.ConfigParser()实例化读取配置方法
+        conifg.read(os.path.join(os.environ['HOME'], 'iselenium.ini'))  # 设置读取配置，设置起始路径和配置文件
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def setUp(self):
+        # config = self.get_config()
+
+        #  控制是否采用无界面形式运行自动化测试
+        try:
+            using_headless = os.environ['using_headless']
+        except KeyError:
+            using_headless = None
+            print('没有配置环境变量 using_headless,按照有界面方式运行自动化测试')
+
+        chrome_options = Options()
+        if using_headless is not None and using_headless.lower() == 'ture':
+            print('使用无界面方式运行')
+            chrome_options.add_argument('--headless')
+
+        self.driver = webdriver.Chrome()
+
+    def test_webui_1(self):
+        """
+        测试用例1，验证‘今日头条’关键词在百度上的搜索结果
+        """
+        self._test_baidu('今日头条', 'test_webui_1')
+
+    def test_webui_2(self):
+        """
+        测试用例2，验证’王者荣耀‘关键词在百度上的搜索结果
+        """
+        self._test_baidu('王者荣耀', 'test_webui_2')
+
+    def _test_baidu(self, search_keyword, testcase_name):
+        self.driver.get("https://www.baidu.com")
+        print('打开浏览器, 访问 www.baidu.com')
+        time.sleep(5)
+        assert f'百度一下' in self.driver.title
+
+        elem = self.driver.find_element_by_name("wd")
+        elem.send_keys(f'{search_keyword}{Keys.RETURN}')  # 输入信息，Keys.RETURN按回车键进入搜索的页面
+        print(f'搜索关键词~{search_keyword}')
+        time.sleep(5)
+        self.assertTrue(f'{search_keyword}' in self.driver.title, msg=f'{testcase_name}校验点 pass')
