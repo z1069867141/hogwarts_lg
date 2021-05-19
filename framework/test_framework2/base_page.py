9939c1063ef3304baddab54886219f960bd64072
z1069867141 <919824370@qq.com>
2021-05-19 18:12:27 +0800

    po的数据驱动

--- framework/test_framework2/base_page.py
+++ framework/test_framework2/base_page.py
@@ -1,5 +1,9 @@
+import logging
+
+import yaml
 from appium import webdriver
 from appium.webdriver.webdriver import WebDriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
 
@@ -29,3 +33,27 @@ class BasePage:
     def click(self):
         self._current_element.click()
         return self
+
+    def send_keys(self, value):
+        self._current_element.send_keys(value)
+
+    def po_run(self, po_method):
+        #read yaml
+        with open('page_demo.yaml') as f:
+            yaml_data = yaml.safe_load(f)
+            #find search
+            for step in yaml_data[po_method]:
+                # find by click send_keys
+                if isinstance(step, dict):
+                    #id click send_keys
+                    for key in step.keys():
+                        if key == "id":
+                            locator = (By.ID, step[key])
+                            self.find(locator)
+                        elif key == "click":
+                            self.click()
+                        elif key == "send_keys":
+                            self.send_keys(step[key])
+                        #todo: 更多关键词
+                        else:
+                            logging.error(f"don't know {step}")
--- framework/test_framework2/demo_page.py
+++ framework/test_framework2/demo_page.py
@@ -11,5 +11,5 @@ class demo(BasePage):
         pass
 
     def search(self, keyword):
-        self.find(self._search_button).click()
+        self.po_run('search')
         return self
