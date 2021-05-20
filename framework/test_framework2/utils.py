bf2000b41f1ef21e26eaf5ed75ae3ced26bc4635
z1069867141 <919824370@qq.com>
2021-05-20 18:15:03 +0800

    po的测试数据驱动

---
+++ framework/test_framework2/utils.py
@@ -0,0 +1,25 @@
+import yaml
+
+
+class Utils:
+    @classmethod
+    def from_file(cls, path):
+        with open(path, encoding="utf8") as f:
+            yaml_data = yaml.safe_load(f)
+            params = yaml_data['params1']
+            keys = set()
+            values = []
+            if isinstance(params, list):
+                for row in params:
+                    if isinstance(row, dict):
+                        for key in row.keys():
+                            keys.add(key)
+                            values.append(row.values())
+            var_names = ','.join(keys)
+            return print({'keys': var_names, 'values': values})
+
+
+if __name__ == "__main__":
+    Utils.from_file("test_search.yaml")
+# {'keys': {'keyword'}, 'values': [dict_values(['alibaba']), dict_values(['baidu']), dict_values(['jd'])]}
+# {'keys': 'keyword', 'values': [dict_values(['alibaba']), dict_values(['baidu']), dict_values(['jd'])]}
